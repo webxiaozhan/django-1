637e1cf8d19e77d3e8b8ba9e10c4fd20a310ae4e
Zhan <2637061150@qq.com>
2018-08-22 14:35:32 +0800

    备注信息

---
+++ project/test1/booktest/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.db.models import Max,F,Q
+from .models import *
+def index(request):
+    # list=BookInfo.books1.filter(heroinfo__hcontent__contains='六')
+    # list=BookInfo.books1.filter(pk__lte=3)
+    # Max1=BookInfo.books1.aggregate(Max('bpub_date'))
+    # list=BookInfo.books1.filter(bread__gt=F('bcommet'))
+    # list=BookInfo.books1.filter(pk__lt=4,btitle__contains='1')
+    # list=BookInfo.books1.filter(pk__lt=4).filter(btitle__contains='1')
+    list=BookInfo.books1.filter(Q(pk__lt=4)|Q(btitle__contains='1'))
+    context={'list1':list
+             # ,'Max1':Max1ss
+             }
+    return render(request,'booktest/index.html',context)
+#from django.http import *
+#def index(request):
+#    return HttpResponse('sss')
